2abd3b53e580c44c79573db65b88c479e883cab6
Derek Hudgins <hudgins.derek@gmail.com>
2021-05-03 17:11:27 -0700

    added frame work and JS for quiz

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-// import functions and grab DOM elements
-
+import { checkForYes } from './utills.js';
+const startQuiz = document.getElementById ('start-quiz');
+const showResult = document.getElementById ('display-results');
 // initialize state
 
 // set event listeners to update state and DOM
+
+startQuiz.addEventListener('click', () => {
+    alert('you are about to take a quiz!');
+    const userQuiz = confirm('Are you ready to take the quiz?');
+    if (userQuiz === false){
+        return;
+    }
+    const first = prompt('whats your first name??');
+    const last = prompt('whats your last name??');
+    const name = first + last;
+    let right = 0;
+    const answer1 = prompt('Do I like to skateboard?');
+    if (checkForYes(answer1)){
+        right = right + 1;}
+    const answer2 = prompt('Have I been to Germany?');
+    if (checkForYes(answer2)){
+        right = right + 1;}
+  
+    const answer3 = prompt('Have I been to South Korea?');
+    if (checkForYes(answer3)){
+        right = right + 1;
+  
+    }
+  
+    showResult.textContent = `Congratulations ${name}! You got ${right} out of 3!`;
+});
